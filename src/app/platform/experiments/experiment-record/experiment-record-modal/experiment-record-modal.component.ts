67645b2e64065a81bf7a626c89f503f88edd114b
CungNguyen <cung.nguyen@hdwebsoft.co>
2021-09-29 15:13:57 +0700

    fix lint

--- src/app/platform/experiments/experiment-record/experiment-record-modal/experiment-record-modal.component.ts
+++ src/app/platform/experiments/experiment-record/experiment-record-modal/experiment-record-modal.component.ts
@@ -110,8 +110,7 @@ export class ExperimentRecordModalComponent implements OnInit {
   }
 
   exportToWord(element, filename = ''): void {
-    const preHtml =
-      "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
+    const preHtml = `<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>`;
     const postHtml = '</body></html>';
     const html = preHtml + document.getElementById(element).innerHTML + postHtml;
     const url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
