3a553ea4531adf35dc0ba2db2c0416926aeb25d4
namth <namth@hdwebsoft.com>
2024-01-24 14:58:32 +0700

    hetzner

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  gatewayBaseUrl: 'https://api.cobalt.origamilab.ch/',
-  padUrl: 'https://etherpad.cobalt.origamilab.ch/api/1/',
+  gatewayBaseUrl: 'https://api.cobalteln.com/',
+  padUrl: 'https://etherpad.cobalteln.com/api/1/',
   authToken: {
     name: 'co_token'
   },
   apiKey: '73e75ae01759d9312f5f6eb7ebb0efb0f3e8af52025dc8cdd508bbeea1de6290',
   padGroupId: 'g.at3YEpNez6r2svU5',
-  chemistryURL: 'https://stoichiometry.cobalt.origamilab.ch/api/webapi/all'
+  chemistryURL: 'https://stoichiometry.cobalteln.com/api/webapi/all'
 };
